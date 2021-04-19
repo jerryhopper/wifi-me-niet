1f1589a26a408492f1049fe656437eb6500a89cd
jerryhopper <hopper.jerry@gmail.com>
2021-04-19 02:11:59 +0200

    initial

--- client/notify.py
+++ client/notify.py
@@ -5,7 +5,7 @@ def notify(message, importance, thetype):
     c = httplib.HTTPSConnection("wifi-me-niet.jerryhopper.com:443")
     headers = { "content-type": "", "pragma": "no-cache" , "cache-control": "no-cache" , "accept": "application/json, */*;q=0.1" ,  "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36" }
     params = urllib.urlencode({'mac-wifi': message})
-    c.request("POST", "/api/sensor",params, headers)
+    c.request("POST", "/api/donotfollow/"+message,params, headers)
     response = c.getresponse()
     print response.status, response.reason
     data = response.read()
